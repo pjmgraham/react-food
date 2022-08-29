ca6d551006957928c680c59915c040c1133554f2
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-08-29 23:18:54 +0200

    adds button component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,21 @@
 import React from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpeg";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
-  return <>
-  <header className={classes.header}>
-    <h1>React Meals!</h1>
-    <button>Cart</button>
-  </header>
-  <div className={classes['main-image']}>
-    <img src={mealsImage} alt="Delicious fod!" />
-  </div>
-  </>
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals!</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Delicious fod!" />
+      </div>
+    </>
+  );
 };
 
 export default Header;
